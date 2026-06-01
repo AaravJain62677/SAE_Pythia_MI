77bb5cb87421bc03c97f4cfb29ab3f5a2e8baa67
AaravJain62677 <aaravjain366@gmail.com>
2026-06-02 01:04:47 +0530

    Made some changes after finetuning the 1st SAE

--- 03_train_sae_finetuned.py
+++ 03_train_sae_finetuned.py
@@ -1,4 +1,5 @@
 import torch
+from transformer_lens import HookedTransformer
 from sae_lens import (
     LanguageModelSAERunnerConfig,
     LanguageModelSAETrainingRunner,
@@ -6,20 +7,22 @@ from sae_lens import (
     LoggingConfig,
 )
 
-#  Device 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using device: {device}")
 
-# Config 
 D_MODEL = 768
 EXPANSION = 8
 L1_COEFF = 8
+FINETUNED_MODEL_PATH = "checkpoints/pythia_finetuned"
+print("Loading fine-tuned model into TransformerLens...")
+from transformers import AutoModelForCausalLM
+hf_model = AutoModelForCausalLM.from_pretrained(FINETUNED_MODEL_PATH)
 
-FINETUNED_MODEL_PATH = "checkpoints/pythia_finetuned"   
-
+model = HookedTransformer.from_pretrained(
+    "EleutherAI/pythia-160m",
+    hf_model=hf_model,
+    device=device,)
 cfg = LanguageModelSAERunnerConfig(
-
-    #SAE architecture (same as base)
     sae=StandardTrainingSAEConfig(
         d_in=D_MODEL,
         d_sae=D_MODEL * EXPANSION,
@@ -27,17 +30,11 @@ cfg = LanguageModelSAERunnerConfig(
         apply_b_dec_to_input=True,
         normalize_activations="expected_average_only_in",
     ),
-
-    #  Fine-tuned model
-    model_name=FINETUNED_MODEL_PATH,
-    hook_name="blocks.8.hook_resid_post",   
-
-    # Dataset
+    model_name="EleutherAI/pythia-160m",
+    hook_name="blocks.8.hook_resid_post",
     dataset_path="Skylion007/openwebtext",
     is_dataset_tokenized=False,
     streaming=True,
-
-    # Training
     training_tokens=5_000_000,
     train_batch_size_tokens=4096,
     context_size=128,
@@ -46,25 +43,12 @@ cfg = LanguageModelSAERunnerConfig(
     lr=5e-5,
     dtype="float32",
     seed=42,
-
-    #Logging            
     logger=LoggingConfig(log_to_wandb=False),
-
-    # Output
     checkpoint_path="checkpoints/sae_finetuned",
     n_checkpoints=3,
-
     device=device,
 )
 
-# Train
 print("\n Training SAE on FINE-TUNED Pythia-160M")
-print(f"  Model:    {FINETUNED_MODEL_PATH}")
-print(f"  Hook:     blocks.8.hook_resid_post")
-print(f"  Features: {D_MODEL * EXPANSION}")
-print(f"  Dataset:  OpenWebText \n")
-
-sae = LanguageModelSAETrainingRunner(cfg).run()
-
-print("\n SAE saved to checkpoints/sae_finetuned/")
-print("Next: run 04_compare_features.py")
+sae = LanguageModelSAETrainingRunner(cfg, model=model).run()
+print("\nSAE Saved to Checkpoint")
